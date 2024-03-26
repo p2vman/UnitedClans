7e294c75244b5541bbc79c24e73b88588e3561b2
MusiJVR <mousejava102@gmail.com>
2024-03-26 21:20:32 +0400

    Beta version 1.0.22

--- src/main/java/unitedclans/hooks/PlaceholderAPIHook.java
+++ src/main/java/unitedclans/hooks/PlaceholderAPIHook.java
@@ -13,6 +13,7 @@ import unitedclans.utils.SqliteDriver;
 
 import java.util.*;
 
+
 public class PlaceholderAPIHook extends PlaceholderExpansion {
 
     private final JavaPlugin plugin;
@@ -47,35 +48,83 @@ public class PlaceholderAPIHook extends PlaceholderExpansion {
             if (params.equals("clanName")) {
                 try {
                     List<Map<String, Object>> rsClanID = sql.sqlSelectData("ClanID", "PLAYERS", "UUID = '" + player.getUniqueId() + "'");
-                    Integer ClanID = (Integer) rsClanID.get(0).get("ClanID");
+                    Integer clanID = (Integer) rsClanID.get(0).get("ClanID");
 
-                    if (ClanID != 0) {
-                        List<Map<String, Object>> rsClan = sql.sqlSelectData("ClanName, ClanColor", "CLANS", "ClanID = " + ClanID);
-                        String ClanName = (String) rsClan.get(0).get("ClanName");
-                        String ClanColor = (String) rsClan.get(0).get("ClanColor");
+                    if (clanID != 0) {
+                        List<Map<String, Object>> rsClanName = sql.sqlSelectData("ClanName, ClanColor", "CLANS", "ClanID = " + clanID);
+                        String clanName = (String) rsClanName.get(0).get("ClanName");
+                        String clanColor = (String) rsClanName.get(0).get("ClanColor");
 
-                        return ChatColor.valueOf(ClanColor) + (ChatColor.BOLD + ClanName + ChatColor.RESET) + " ";
+                        return ChatColor.valueOf(clanColor) + (ChatColor.BOLD + clanName + ChatColor.RESET) + " ";
                     }
 
                     return "";
                 } catch (Exception e) {
                     System.err.println(e.getClass().getName() + ": " + e.getMessage());
                 }
-            } else if (params.equals("clanRole")) {
+            } else if (params.equals("clanCountMembers")) {
                 try {
-                    List<Map<String, Object>> rsClanID = sql.sqlSelectData("ClanRole", "PLAYERS", "UUID = '" + player.getUniqueId() + "'");
-                    String ClanRole = (String) rsClanID.get(0).get("ClanRole");
+                    List<Map<String, Object>> rsClanID = sql.sqlSelectData("ClanID", "PLAYERS", "UUID = '" + player.getUniqueId() + "'");
+                    Integer clanID = (Integer) rsClanID.get(0).get("ClanID");
 
-                    if (!ClanRole.equals(UnitedClans.getInstance().getConfig().getString("roles.no-clan"))) {
+                    if (clanID != 0) {
+                        List<Map<String, Object>> rsClanCountMembers = sql.sqlSelectData("CountMembers", "CLANS", "ClanID = " + clanID);
+                        Integer countMembers = (Integer) rsClanCountMembers.get(0).get("CountMembers");
+
+                        return countMembers.toString() + " ";
+                    }
+
+                    return "";
+                } catch (Exception e) {
+                    System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                }
+            } else if (params.equals("clanKills")) {
+                try {
+                    List<Map<String, Object>> rsClanID = sql.sqlSelectData("ClanID", "PLAYERS", "UUID = '" + player.getUniqueId() + "'");
+                    Integer clanID = (Integer) rsClanID.get(0).get("ClanID");
+
+                    if (clanID != 0) {
+                        List<Map<String, Object>> rsClanKills = sql.sqlSelectData("Kills", "CLANS", "ClanID = " + clanID);
+                        Integer clanKills = (Integer) rsClanKills.get(0).get("Kills");
+
+                        return ChatColor.DARK_RED + clanKills.toString() + "\uD83D\uDDE1" + " ";
+                    }
+
+                    return "";
+                } catch (Exception e) {
+                    System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                }
+            } else if (params.equals("clanBank")) {
+                try {
+                    List<Map<String, Object>> rsClanID = sql.sqlSelectData("ClanID", "PLAYERS", "UUID = '" + player.getUniqueId() + "'");
+                    Integer clanID = (Integer) rsClanID.get(0).get("ClanID");
+
+                    if (clanID != 0) {
+                        List<Map<String, Object>> rsClanBank = sql.sqlSelectData("Bank", "CLANS", "ClanID = " + clanID);
+                        Integer clanBank = (Integer) rsClanBank.get(0).get("Bank");
+
+                        return ChatColor.DARK_GREEN + clanBank.toString() + "$" + " ";
+                    }
+
+                    return "";
+                } catch (Exception e) {
+                    System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                }
+            } else if (params.equals("playerRole")) {
+                try {
+                    List<Map<String, Object>> rsPlayerRole = sql.sqlSelectData("ClanRole", "PLAYERS", "UUID = '" + player.getUniqueId() + "'");
+                    String playerRole = (String) rsPlayerRole.get(0).get("ClanRole");
+
+                    if (!playerRole.equals(UnitedClans.getInstance().getConfig().getString("roles.no-clan"))) {
                         String color = "WHITE";
                         String role = "";
-                        if (ClanRole.equals(UnitedClans.getInstance().getConfig().getString("roles.member"))) {
+                        if (playerRole.equals(UnitedClans.getInstance().getConfig().getString("roles.member"))) {
                             color = "GREEN";
                             role = LocalizationUtils.langCheck(language, "MEMBER");
-                        } else if (ClanRole.equals(UnitedClans.getInstance().getConfig().getString("roles.elder"))) {
+                        } else if (playerRole.equals(UnitedClans.getInstance().getConfig().getString("roles.elder"))) {
                             color = "GOLD";
                             role = LocalizationUtils.langCheck(language, "ELDER");
-                        } else if (ClanRole.equals(UnitedClans.getInstance().getConfig().getString("roles.leader"))) {
+                        } else if (playerRole.equals(UnitedClans.getInstance().getConfig().getString("roles.leader"))) {
                             color = "DARK_PURPLE";
                             role = LocalizationUtils.langCheck(language, "LEADER");
                         }
@@ -83,6 +132,24 @@ public class PlaceholderAPIHook extends PlaceholderExpansion {
                     }
 
                     return "";
+                } catch (Exception e) {
+                    System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                }
+            } else if (params.equals("playerKills")) {
+                try {
+                    List<Map<String, Object>> rsPlayerKills = sql.sqlSelectData("Kills", "PLAYERS", "UUID = '" + player.getUniqueId() + "'");
+                    Integer playerKills = (Integer) rsPlayerKills.get(0).get("Kills");
+
+                    return ChatColor.DARK_RED + playerKills.toString() + "\uD83D\uDDE1" + " ";
+                } catch (Exception e) {
+                    System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                }
+            } else if (params.equals("playerDonations")) {
+                try {
+                    List<Map<String, Object>> rsPlayerDonations = sql.sqlSelectData("Donations", "PLAYERS", "UUID = '" + player.getUniqueId() + "'");
+                    Integer playerDonations = (Integer) rsPlayerDonations.get(0).get("Donations");
+
+                    return ChatColor.DARK_RED + playerDonations.toString() + "$" + " ";
                 } catch (Exception e) {
                     System.err.println(e.getClass().getName() + ": " + e.getMessage());
                 }
